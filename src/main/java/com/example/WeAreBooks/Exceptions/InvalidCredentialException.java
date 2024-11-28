dcc2de7a8c5cf6eb96431be737ad412eed702c85
U.H. Chathurya Hewage <111672583+hewageuhcu@users.noreply.github.com>
2024-11-28 20:57:25 +0530

    exception added

--- src/main/java/com/example/WeAreBooks/Exceptions/InvalidCredentialException.java
+++ src/main/java/com/example/WeAreBooks/Exceptions/InvalidCredentialException.java
@@ -1,6 +1,6 @@
 package com.example.WeAreBooks.Exceptions;
 
-public class InvalidCredentialException extends RuntimeException {
+public class InvalidCredentialException extends  RuntimeException {
     public InvalidCredentialException(String s) {
         super(s);
     }
