5d431488bb4607566d9f82a5fb80ede361ebc6d4
U.H. Chathurya Hewage <111672583+hewageuhcu@users.noreply.github.com>
2024-11-28 20:47:02 +0530

    responses added

--- src/main/java/com/example/WeAreBooks/DTO/ReviewResponse.java
+++ src/main/java/com/example/WeAreBooks/DTO/ReviewResponse.java
@@ -4,8 +4,8 @@ public class ReviewResponse {
     public String id;
     public String bookId;
     public String userId;
-    public String user;
     public String date;
     public String review;
     public Short rate;
+    public String user;
 }
